2d9725767b2de661666e257e3ceb16613ba7679d
leworkalush <81740368+leworkalush@users.noreply.github.com>
2023-10-24 04:23:52 +0300

    Create webpack.config.js

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
